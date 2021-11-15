69e097b73ce2a725ccff04d834f68e7aeb5523aa
Christopher Norman <christopherjohnnorman@gmail.com>
2021-11-15 23:56:10 +0000

    Partially implement initial state and comments Code comments and function descriptions Starting to implement initialisation for state sequence generation for Markov chains

--- MarkovChain.py
+++ MarkovChain.py
@@ -2,6 +2,15 @@ import numpy as np
 
 
 class MarkovChain(object):
+    """
+    Class for handling Markov chain generation and sequence generation
+    transition_matrix: 2D array containing probabilities of transitioning
+    order: integer describing the order of the Markov chain
+    states: list of strings which contain the names of the nodes within the Markov chain
+    index_dict: dictionary to retrieve a states string name from an integer index ID
+    state_dict: dictionary to retrieve a states integer ID from a states name as a string
+    initial_probability: probability vector for choosing the initial state of the model
+    """
     def __init__(self, order: int, data: list):
         self.transition_matrix = np.atleast_2d
         self.order = order
@@ -9,8 +18,14 @@ class MarkovChain(object):
         self.states = []
         self.index_dict = {}
         self.state_dict = {}
+        self.initial_probability = []
 
     def create_transition_matrix(self):
+        """
+        Creates a transition matrix containing probabilities of transitioning from one node
+        on a row to a node in the corresponding column. Also creates an initial probability vector.
+        :return:
+        """
         for i in range(len(self.data) - self.order):
             state_name = str(self.data[i])
             for j in range(1, self.order):
@@ -24,6 +39,8 @@ class MarkovChain(object):
         self.state_dict = {index: self.states[index] for index in range(number_states)}
         self.transition_matrix = np.zeros(shape=(number_states, number_states))
 
+        self.initial_probability = np.zeros(shape=number_states)  # Initial state probability vector
+
         current_state = self.states[0]
         next_state = ""
 
@@ -35,16 +52,24 @@ class MarkovChain(object):
             current_state_index = self.index_dict[current_state]
             next_state_index = self.index_dict[next_state]
             self.transition_matrix[current_state_index][next_state_index] += 1
+
+            # Add 1 to initial probability vector when current state appears
+            self.initial_probability[current_state_index] += 1
             current_state = next_state
 
         for row in range(self.transition_matrix.shape[0]):
             if np.sum(self.transition_matrix[row]) == 0:  # Node has zero probability so add 1 to each row entry
                 self.transition_matrix[row] = 1  # Might change this in future
             self.transition_matrix[row] /= np.sum(self.transition_matrix[row])
-        print(self.states)
-        print(self.transition_matrix)
+
+        self.initial_probability /= np.sum(self.initial_probability)
 
     def next_state(self, current_state: str) -> str:
+        """
+        Chooses the next state based on the current state
+        :param current_state: name of the current node in sequence
+        :return: next_state: random next state in sequence based on probabilities in the transition matrix
+        """
         next_state = np.random.choice(self.states, p=self.transition_matrix[self.index_dict[current_state], :])
         return next_state
 
